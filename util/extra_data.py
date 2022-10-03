2bae42cd186d6179ffb52f117d6784325626ad7e
zapagenrevdale <zapagenrevdale@gmail.com>
2022-10-03 13:42:41 +0800

    Include extra data

---
+++ util/extra_data.py
@@ -0,0 +1,189 @@
+background_url_data = {
+    "tt0371746": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Iron_Man_1_uaa2rz.jpg",
+    "tt0800080": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026164/marvels/The_Incredible_Hulk_k8noka.jpg",
+    "tt1228705": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Iron_Man_2_ltusf2.jpg",
+    "tt0800369": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/Thor_oa3cya.jpg",
+    "tt0458339": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Captain_America_-_The_First_Avenger_nblbog.jpg",
+    "tt0848228": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026170/marvels/The_Avengers_uemtbz.jpg",
+    "tt1300854": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Iron_Man_3_kfzzjz.jpg",
+    "tt1981115": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Thor_-_The_Dark_World_tdeoqy.jpg",
+    "tt1843866": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Captain_America_-_The_Winter_Soldier_nw2tw9.jpg",
+    "tt2015381": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Guardians_of_the_Galaxy_hegzyt.jpg",
+    "tt2395427": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Avengers_Age_of_Ultron_jqbj4o.jpg",
+    "tt0478970": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Ant-Man_eflori.jpg",
+    "tt3498820": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Captain_America_Civil_War_zg3716.jpg",
+    "tt1211837": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Doctor_Strange_zyg8k5.jpg",
+    "tt3896198": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Guardians_of_the_Galaxy_Vol._2_yiadrl.jpg",
+    "tt2250912": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Spider-Man_Homecoming_hlminr.jpg",
+    "tt3501632": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/Thor_Ragnarok_cs6opv.jpg",
+    "tt1825683": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Black_Panther_jhgsmu.jpg",
+    "tt4154756": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Avengers_Infinity_War_nerfbz.jpg",
+    "tt5095030": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/Ant-Man_and_The_Wasp_ns7eqi.jpg",
+    "tt4154664": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Captain_Marvel_lormqf.jpg",
+    "tt4154796": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Avengers_Endgame_kl0ncg.jpg",
+    "tt6320628": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Spider-Man_Far_From_Home_ujvz28.jpg",
+    "tt3480822": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026094/marvels/Black_Widow_inzqi3.jpg",
+    "tt9376612": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Shang-Chi_and_The_Legend_of_The_Ten_Rings_qozyne.jpg",
+    "tt9032400": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Eternals_sjep7z.jpg",
+    "tt10872600": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026169/marvels/Spider-Man_No_Way_Home_mqniwr.jpg",
+    "tt9419884": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Doctor_Strange_in_the_Multiverse_of_Madness_s1fv6b.jpg",
+    "tt10648342": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026170/marvels/Thor_Love_and_Thunder_xtnskq.png",
+    "tt9114286": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Black_Panther_Wakanda_Forever_m3etqx.jpg",
+    "tt10954600": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/Ant-Man_and_The_Wasp_Quantumania_ciiohu.jpg",
+    "tt6791350": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026168/marvels/Guardians_of_the_Galaxy_Vol._3_xpcpjd.jpg",
+    "tt10676048": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/The_Marvels_xaxn6q.jpg",
+    "tt10671440": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Blade_xbnajh.jpg",
+    "tt14513804": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Captain_America_New_World_Order_wz79xw.jpg",
+    "tt20969586": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026165/marvels/Thunderbolts_jwbah7.webp",
+    "tt10676052": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662027671/marvels/Fantastic_Four_cj6w4b.webp",
+    "tt21361438": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026167/marvels/Avengers_The_Kang_Dynasty_ecsplm.jpg",
+    "tt21361444": "https://res.cloudinary.com/genrev-zapa/image/upload/v1662026166/marvels/Avengers_Secret_Wars_mpccui.webp",
+}
+
+stream_sites_data = {
+    "tt0371746": {
+        "Youtube": "https://youtu.be/gOe5jcSmQT4",
+        "Apple TV": "https://tv.apple.com/ph/movie/iron-man/umc.cmc.219lnb4s678o8roez3o905h7r?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=gOe5jcSmQT4"
+    },
+    "tt0800080": {
+        "Youtube": "https://youtu.be/Dtp_bO8H29Y",
+        "Apple TV": "https://tv.apple.com/ph/movie/the-incredible-hulk-2008/umc.cmc.1uldcoh87etoxgugkmqur4pvp?action=play",
+        "Google Play": "https://play.google.com/store/movies/details/The_Incredible_Hulk?id=5OPDu0hp7A8&hl=en&gl=US"
+    },
+    "tt1228705": {
+        "Youtube": "https://youtu.be/5wSFCEiHa28",
+        "Apple TV": "https://tv.apple.com/ph/movie/iron-man-2/umc.cmc.1y2bgb9s3j4an0b1bra6ekv3s?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=5wSFCEiHa28"
+    },
+    "tt0800369":  {
+        "Youtube": "https://youtu.be/PP6ZIrPxwH8",
+        "Apple TV": "https://tv.apple.com/ph/movie/thor/umc.cmc.hgkuacmdh6wxtlwtwpuju43?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=PP6ZIrPxwH8"
+    },
+    "tt0458339": {
+        "Youtube": "https://youtu.be/XaUPjTLhwFc",
+        "Apple TV": "https://tv.apple.com/ph/movie/captain-america-the-first-avenger/umc.cmc.621ts42h8b35wrzkmdlqiozsw?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=XaUPjTLhwFc"
+    },
+    "tt0848228": {
+        "Youtube": "https://youtu.be/xtdKXEI-gc8",
+        "Apple TV": "https://tv.apple.com/ph/movie/the-avengers/umc.cmc.7i757ymuvffa02onn3uva0emx?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=CkgbNEKFMfo"
+    },
+    "tt1300854": {
+        "Youtube": "https://youtu.be/Gjsb6uhjbkc",
+        "Apple TV": "https://tv.apple.com/ph/movie/iron-man-3/umc.cmc.330w7tm79l0jupdtwihclt1rh?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=gthrePq-YSY.P"
+    },
+    "tt1981115": {
+        "Youtube": "https://youtu.be/DpSTUUvvMfs",
+        "Apple TV": "https://tv.apple.com/ph/movie/thor-the-dark-world/umc.cmc.4g07e0agi9p1c2puo15jz89zo?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=DpSTUUvvMfs"
+    },
+    "tt1843866": {
+        "Youtube": "https://youtu.be/Lh23yDUiFtQ",
+        "Apple TV": "https://tv.apple.com/ph/movie/captain-america-the-winter-soldier/umc.cmc.5nqu67oqx22vpun5h4zc2w2lu?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=Lh23yDUiFtQ"
+    },
+    "tt2015381": {
+        "Youtube": "https://youtu.be/P2Tarm8edGo",
+        "Apple TV": "https://tv.apple.com/ph/movie/guardians-of-the-galaxy/umc.cmc.71igdwen6ko2dlhwaw12vbdh?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=P2Tarm8edGo"
+    },
+    "tt2395427": {
+        "Youtube": "https://youtu.be/2PfKg6R3m34",
+        "Apple TV": "https://tv.apple.com/ph/movie/avengers-age-of-ultron/umc.cmc.scq1juwtw6cfpo92aot2c5qx?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=2PfKg6R3m34"
+    },
+    "tt0478970":  {
+        "Youtube": "https://youtu.be/2HG_nY1ygYg",
+        "Apple TV": "https://tv.apple.com/ph/movie/ant-man/umc.cmc.5v0c1mtw53zkyw8a0hf8xglwm?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=2HG_nY1ygYg"
+    },
+    "tt3498820": {
+        "Youtube": "https://youtu.be/Vy6EGCToeVw",
+        "Apple TV": "https://tv.apple.com/ph/movie/captain-america-civil-war/umc.cmc.6gq2ehunu5q6zvh9up3cw2i0d?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=Vy6EGCToeVw"
+    },
+    "tt1211837": {
+        "Youtube": "https://youtu.be/SPD0fSV4qJ8",
+        "Apple TV": "https://tv.apple.com/ph/movie/doctor-strange-2016/umc.cmc.1pr69aixggljcme3mjh826zj6?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=SPD0fSV4qJ8"
+    },
+    "tt3896198": {
+        "Youtube": "https://youtu.be/3SWsToYMFAs",
+        "Apple TV": "https://tv.apple.com/ph/movie/guardians-of-the-galaxy-vol-2/umc.cmc.7g0gtypmo4kimdjqo0x078kuk?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=3SWsToYMFAs"
+    },
+    "tt2250912":  {
+        "Youtube": "https://youtu.be/rk-dF1lIbIg",
+        "Apple TV": "https://tv.apple.com/ph/movie/spider-man-homecoming/umc.cmc.2sbgqu6gpbevxnjjhfd4ahuvn?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=vaYRC8mIusY"
+    },
+    "tt3501632": {
+        "Youtube": "https://youtu.be/TUDcwxLC4Pc",
+        "Apple TV": "https://tv.apple.com/ph/movie/thor-ragnarok/umc.cmc.5f4fq6k3njmustywcg5jld7im?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=TUDcwxLC4Pc"
+    },
+    "tt1825683":  {
+        "Youtube": "https://youtu.be/TUDcwxLC4Pc",
+        "Apple TV": "https://tv.apple.com/ph/movie/thor-ragnarok/umc.cmc.5f4fq6k3njmustywcg5jld7im?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=TUDcwxLC4Pc"
+    },
+    "tt4154756": {
+        "Youtube": "https://youtu.be/MCdf1wSsYxk",
+        "Apple TV": "https://tv.apple.com/ph/movie/avengers-infinity-war/umc.cmc.2do0xuo1u4tjmqbfz1p42a0p9?action=play",
+        "Google Play": "https://play.google.com/store/movies/details/Marvel_Studios_Avengers_Infinity_War?id=OxzKb4a1Qc4&hl=en_AU&gl=US"
+    },
+    "tt5095030": {
+        "Youtube": "https://youtu.be/oV5zdOs9saQ",
+        "Apple TV": "https://tv.apple.com/ph/movie/ant-man-and-the-wasp/umc.cmc.14uirj9rqpjg8prk3joqzfaif?action=play",
+        "Google Play": "hhttps://play.google.com/store/movies/details?id=oV5zdOs9saQ"
+    },
+    "tt4154664": {
+        "Youtube": "https://youtu.be/A4IrqcXUvSY",
+        "Apple TV": "https://tv.apple.com/ph/movie/captain-marvel/umc.cmc.3o1o52plk3pxcpvhfhdrzrsun?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=A4IrqcXUvSY"
+    },
+    "tt4154796": {
+        "Youtube": "https://youtu.be/o0yFu-JU0UU",
+        "Apple TV": "https://tv.apple.com/ph/movie/marvel-studios-avengers-endgame/umc.cmc.4ao9tm6b6rz4sy7yj5v13ltf8?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=o0yFu-JU0UU"
+    },
+    "tt6320628": {
+        "Youtube": "https://youtu.be/8gMUBhQ3P9Y",
+        "Apple TV": "https://tv.apple.com/ph/movie/spider-man-far-from-home/umc.cmc.1xnsjqmhn8a93iy2vgj36w3d1?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=3RbZaR_rgUc.P"
+    },
+    "tt3480822": {
+        "Youtube": "https://youtu.be/n_3IzXHZP2Y",
+        "Apple TV": "https://tv.apple.com/ph/movie/black-widow-2021/umc.cmc.3tfe3lm0qa0b29a6r84hc8jjy?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=Jf0x76Zv4eY.P"
+    },
+    "tt9376612": {
+        "Youtube": "https://youtu.be/GBfCnkQ8yhg",
+        "Apple TV": "https://tv.apple.com/ph/movie/shang-chi-and-the-legend-of-the-ten-rings/umc.cmc.1r6h2ku90p1e6jaznhnk0m5hd?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=Obtg9BpYF84.P"
+    },
+    "tt9032400": {
+        "Youtube": "https://youtu.be/fP8tTkzzWOk",
+        "Apple TV": "https://tv.apple.com/ph/movie/eternals/umc.cmc.7gst8rnk6n9w3ac567v4mm687?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=4cDE61O4Q1c.P"
+    },
+    "tt10872600": {
+        "Youtube": "https://youtu.be/sgpfaz0lN7g",
+        "Apple TV": "https://tv.apple.com/ph/movie/spider-man-no-way-home/umc.cmc.2qf7xc5hds0m5jgx4roago580?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=8sEo_7rbXb8.P"
+    },
+    "tt9419884": {
+        "Youtube": "https://youtu.be/Pc5ORo3bFOM",
+        "Apple TV": "https://tv.apple.com/ph/movie/doctor-strange-in-the-multiverse-of-madness/umc.cmc.5f0mydwzevgd47pqxidz6sjf9?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=ra-EpkNpemk.P"
+    },
+    "tt10648342": {
+        "Youtube": "https://youtu.be/Pd25wQrGZ40",
+        "Apple TV": "https://tv.apple.com/ph/movie/thor-love-and-thunder/umc.cmc.3pqhs9la0chxz6ap14b10gvul?action=play",
+        "Google Play": "https://play.google.com/store/movies/details?id=mJ2eRHbyUVI.P"
+    },
+}
