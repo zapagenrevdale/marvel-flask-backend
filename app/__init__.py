8f5bb786def46662f03c261f74d1afc6f223127e
zapagenrevdale <zapagenrevdale@gmail.com>
2022-10-03 13:40:49 +0800

    Create models and schema

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,5 +11,9 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+ma = Marshmallow(app)
+cors = CORS(app)
+
+
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,2 +1,89 @@
+from app import db, ma
 
+class StreamSite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    link = db.Column(db.String(255))
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
+
+class StreamSiteSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "link")
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(255))
+    value = db.Column(db.String(255))
+    imdb_id = db.Column(db.Integer, db.ForeignKey("imdb.id"))
+
+
+class RatingSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "source", "value", "imdb_id")
+
+
+class Imdb(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer)
+    rated = db.Column(db.String(255))
+    genre = db.Column(db.String(255))
+    director = db.Column(db.String(255))
+    actors = db.Column(db.String(255))
+    plot = db.Column(db.Text)
+    language = db.Column(db.String(255))
+    awards = db.Column(db.String(255))
+    metascore = db.Column(db.Integer)
+    rating = db.Column(db.Float(1))
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    ratings = db.relationship("Rating", backref="imdb", lazy="dynamic")
+
+
+
+class ImdbSchema(ma.Schema):
+    class Meta:
+        fields = (
+            "id",
+            "year",
+            "rated",
+            "genre",
+            "director",
+            "actors",
+            "plot",
+            "language",
+            "awards",
+            "metascore",
+            "rating",
+            "movie_id",
+            "ratings",
+        )
+
+    ratings = ma.Nested(RatingSchema(), many=True)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imdb_id = db.Column(db.String(255))
+    title = db.Column(db.String(255), nullable=False)
+    overview = db.Column(db.Text)
+    cover_url = db.Column(db.String(255), nullable=False)
+    background_url = db.Column(db.String(255))
+    trailer_url = db.Column(db.String(255))
+    saga = db.Column(db.String(255))
+    chronology = db.Column(db.Integer)
+    post_credit_scenes = db.Column(db.Integer)
+    phase = db.Column(db.Integer)
+    release_date = db.Column(db.Date)
+    box_office = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+    stream_sites = db.relationship("StreamSite", backref="movie", lazy="subquery")
+    imdb = db.relationship("Imdb", backref="movie", lazy="subquery", uselist=False)
+
+
+
+class MovieSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Movie
+        
+    imdb = ma.Nested(ImdbSchema())
+    stream_sites = ma.Nested(StreamSiteSchema(), many=True)
 
